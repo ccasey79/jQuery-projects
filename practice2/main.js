dfb7c743e47e67d19580d8ba67e69edee876cb23
christian <christiancasey969@gmail.com>
2017-04-08 12:19:15 +0100

    jquery binds

--- practice2/main.js
+++ practice2/main.js
@@ -1,6 +1,23 @@
 
+
+// fade in / out
 $(document).ready(function(){
   $("#logo").on("click", function(){
     $(this).fadeOut(3000).fadeIn(2000);
   });
+
+// bind on / off
+var list = $("h3");
+ list.on("click", function(){
+  $(this).css({ "text-decoration": "underline"});
+  list.off();
+  });
+
+$("#client-head").on("click", function(){
+    $(this).css({"background-color": "#003300"});
+});
+
+
+
+
 });
