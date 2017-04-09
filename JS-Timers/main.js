ffd3b83278c49de2b3a4c25d5a46bd787dac8daa
christian <christiancasey969@gmail.com>
2017-04-09 14:07:35 +0100

    JS timer to fade in element

--- JS-Timers/main.js
+++ JS-Timers/main.js
@@ -1,2 +1,15 @@
 
-console.log("JS Loaded!");
+// Fade in an element using a timer
+// Done by adding a class name to the element eg. message
+
+var myMessage = document.getElementById('message');
+
+function showMessage() {
+
+  myMessage.className = "show";    // add class to element
+
+}
+
+setTimeout(showMessage, 2000);
+
+// calling the showMessage function & setting a time of 3 seconds
