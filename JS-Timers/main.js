f57232d51e1c122ce52c507bde90f4e99917ec44
christian <christiancasey969@gmail.com>
2017-04-09 14:47:06 +0100

    creating & adding link in JS

--- JS-Timers/main.js
+++ JS-Timers/main.js
@@ -11,5 +11,16 @@ function showMessage() {
 }
 
 setTimeout(showMessage, 2000);
+// calling the showMessage function & setting a time of 3 second.
 
-// calling the showMessage function & setting a time of 3 seconds
+
+// creating an element (link), then adding to the dom.
+// adding the href as well to the link.
+// message is the id of the element.
+
+var link = document.createElement("a");
+var box = document.getElementById("message");
+
+link.innerHTML = "Cornish Pasties Buy Here!";
+link.setAttribute("href", "http://google.com");
+box.appendChild(link);
