46ca1b7a3a455bb3b8d6f8f671efd8d211528acf
christian <christiancasey969@gmail.com>
2017-04-12 20:27:16 +0100

    slide & animate effects

--- practice3/menu.js
+++ practice3/menu.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 
     $("#burger-nav").on("click", function(){
-    
+
         $("header nav ul").toggleClass("open");
-        
+
     });
+
+    $(".logo").on("click", function(){
+      $("#main-banner").slideUp(3000);
+    });
+
+    $("#latest-work h2").animate({"margin-left" : "42%"}, 3000);
 
 });
