03ca227741e0a6276f138302b3089d1a31c55a05
christian <christiancasey969@gmail.com>
2017-11-03 19:52:51 +0000

    traversy - jquery selectors

--- traversy-crash-course/main.js
+++ traversy-crash-course/main.js
@@ -1,1 +1,25 @@
 console.log('JS Loaded');
+
+
+// SELECTORS
+
+
+// $('h1').hide();
+// $('h1#heading1').hide();
+//$('h1.heading2').hide();
+
+//$('p span').css('color', 'red');
+
+$('ul#list li:first').css('color', 'red');
+$('ul#list li:last').css('color', 'green');
+$('ul#list li:even').css('background-color', 'yellow');
+$('ul#list li:odd').css('background-color', '#cccccc');
+$('ul#list li:nth-child(3)').css('list-style', 'none');
+
+// $(':button').hide();
+// $(':submit').hide();
+
+$('[href]').css('color', 'red');
+$('a[href="http://yahoo.com"]').css('color', 'green');
+
+//$('*').hide(); 
