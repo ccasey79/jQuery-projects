72055aa09f4dcf27a28aa89ce4196ccad94f3ddf
christian <christiancasey969@gmail.com>
2017-11-04 15:04:29 +0000

    jquery dom manipulation setup

---
+++ traversy-crash-course/dom-manipulation/main.js
@@ -0,0 +1,34 @@
+console.log('JS Loaded!');
+
+$(document).ready(function(){
+
+  // $('p.para1').css('color', ' red'); - one element chosen
+  // $('p.para1').css({color: 'red', background: '#ccc'}); - more than one element chosen, create as an object
+
+  // ADDING / REMOVING CLASSES
+
+  //$('p.para2').addClass('myClass');
+  //$('p.para2').removeClass('myClass');
+  // $('#btn1').click(function(){
+  //   $('p.para2').toggleClass('myClass');
+  // })
+
+  //$('#myDiv').text('Hello World');
+  // $('#myDiv').html('<h3>Hello World</h3>');
+
+  // APPEND / PREPEND
+
+  $('ul').append('<li>Tomato</li>');
+  $('ul').prepend('<li>Apple</li>');
+
+  $('ul').before('<h4>Hello</h4>')
+  $('ul').after('<h4>Goodbye</h4>')
+
+  $('ul').empty();  // makes elements inside disappear
+  //$('ul').detach(); // deletes
+
+
+
+
+
+});
