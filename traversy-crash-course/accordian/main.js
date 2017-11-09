7266c42646f99e10797465c8ee9f6f46809b75f4
christian <christiancasey969@gmail.com>
2017-11-09 18:52:23 +0000

    jquery accordion - functionality added to questions & answers when clicked on

--- traversy-crash-course/accordian/main.js
+++ traversy-crash-course/accordian/main.js
@@ -1,1 +1,31 @@
 console.log('JS Loaded!');
+
+// ACCORDION
+var action = 'click';
+var speed = 500;
+
+$(document).ready(function(){
+
+  // Question handler
+  $('li.q').on(action, function(){
+
+    // Get next element
+    $(this).next()
+      .slideToggle(speed)
+
+        // Select all other answers
+        .siblings('li.a')
+          .slideUp();
+
+  // To rotate the arrow image
+  // Get image for active question
+  var img = $(this).children('img');
+
+  // Remove the 'rotate' class from all images except the active one
+  $('img').not(img).removeClass('rotate');
+
+  // Toggle rotate rotate class
+  img.toggleClass('rotate');
+
+  });
+});
